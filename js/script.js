65e92357454942e8d53062b5589322a51fa36b5e
bellamard <belamard@gmail.com>
2021-04-08 10:07:53 +0100

    commit 9 jours

--- js/script.js
+++ js/script.js
@@ -23,4 +23,40 @@ const navSlive = () => {
 
 
 }
+const language = () => {
+    const lang = document.querySelectorAll('.icon');
+    const bar = document.querySelector('.progress');
+    const comp = document.querySelectorAll('.niveau2')
+    lang.forEach((langu, index) => {
+        langu.addEventListener('click', (event) => {
+            event.preventDefault();
+            bar.classList.toggle('haut');
+            comp.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = ''
+                } else {
+                    link.style.animation = `navLinkfade 0.5s ease forwards ${index / 7 + 0.8}s`;
+                    
+                }
+            });
+        });
+
+    });
+
+}
+const progressbar =() =>{
+    const progress=[60, 96, 90, 92, 98];
+    const pro=document.querySelectorAll('.niveau div');
+    pro.forEach((tab, index)=>{
+        /* if(tab.style.animation){
+            tab.style.animation='';
+        }else{
+            tab.style.animation=''
+        } */
+        console.log(tab.className.set)
+        
+    });
+}
+progressbar();
+language();
 navSlive();

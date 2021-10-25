fc05e18e25962786ea1a64b2d8abd763a58510ce
bellamard <belamard@gmail.com>
2021-10-25 09:01:01 +0100

    feat(fr): créer server json

--- script/Script.js
+++ script/Script.js
@@ -1,6 +1,13 @@
-const name_profil = document.querySelector("#name_profil");
-const img_profil = document.querySelector("#img_profil");
-const icon_link = document.querySelector("#icon_link");
+let name_profil = document.querySelector("#name_profil");
+let img_profil = document.querySelector("#img_profil");
+let icon_link = document.querySelector("#icon_link");
+let frontend = document.querySelector("#frontend");
+let backend = document.querySelector("#backend");
+let bdd = document.querySelector("#bdd");
+let ci = document.querySelector("#ci");
+let autre = document.querySelector("#autre");
+let projets = document.querySelector("#projets");
+
 fetch("http://localhost:3000/profil")
   .then((response) => {
     return response.json();
@@ -34,6 +41,60 @@ fetch("http://localhost:3000/reseaux")
       icon_rx.className = `bi bi-${key.name}`;
       icon_link.appendChild(link);
       link.appendChild(icon_rx);
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+// fetch("http://localhost:3000/competence")
+//   .then((reponse) => {
+//     return reponse.json();
+//   })
+//   .then((competence) => {
+//     for (const key of competence) {
+//       for (const iterator of key.frontend) {
+//         const item_frontend = document.createElement("li");
+//         item_frontend.innerText = iterator.name;
+//         frontend.appendChild(item_frontend);
+//       }
+//       for (const iterator of key.backend) {
+//         const item_backend = document.createElement("li");
+//         item_backend.innerText = iterator.name;
+//         backend.appendChild(item_backend);
+//       }
+//       for (const iterator of key.ci) {
+//         const item_ci = document.createElement("li");
+//         item_ci.innerText = iterator.name;
+//         ci.appendChild(item_ci);
+//       }
+//       for (const iterator of key.autre) {
+//         const item_autre = document.createElement("li");
+//         item_autre.innerText = iterator.name;
+//         autre.appendChild(item_autre);
+//       }
+//       for (const iterator of key.bdd) {
+//         const item_bdd = document.createElement("li");
+//         item_bdd.innerText = iterator.name;
+//         bdd.appendChild(item_bdd);
+//       }
+//     }
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+fetch("http://localhost:3000/projet")
+  .then((reponse) => {
+    return reponse.json();
+  })
+  .then((projet) => {
+    for (const key of projet) {
+      const projet_item = document.createElement("a");
+      projet_item.setAttribute("href", key.url);
+      projet_item.innerText = key.title;
+      // projets.appendChild(projet_item);
+      console.log(key.url);
     }
   })
   .catch((error) => {
--- script/alpine.js
+++ script/alpine.js
@@ -8,5 +8,33 @@ document.addEventListener("alpine:init", () => {
       return tab === this.tab;
     },
   }));
+
+  Alpine.data("competence", () => ({
+    loading: false,
+    backend: [],
+    frontend: [],
+    ci: [],
+    autre: [],
+    bdd: [],
+    loaded: false,
+    async load_Compe() {
+      this.loading = true;
+      fetch("http://localhost:3000/competence")
+        .then((response) => {
+          return response.json();
+        })
+        .then((competence) => {
+          this.backend = competence.backend;
+          this.frontend = competence.frontend;
+          this.ci = competence.ci;
+          this.autre = competence.autre;
+          this.bdd = competence.bdd;
+          this.loaded = true;
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  }));
 });
-console.log("ALPINE OK");
